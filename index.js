8b1e021b603815c8c0d51d32f0257bcd2dceee34
+ <+>
2022-02-07 12:57:45 -0500

    commit tree e94e0cd617a7b9e12ee6a9ea73d255ca96a930c2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const msg = [];
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    msg.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return msg;
+}
+
+function countDown(x) {
+  while (x >= 0) {
+    console.log(x--);
+  }
+}
